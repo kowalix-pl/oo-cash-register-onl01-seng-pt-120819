60ef776613f78ac2c0026307d81571fdb409953a
kowalix007 <kowalix007@gmail.com>
2020-01-25 21:30:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,47 @@
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount, :price, :items, :last_transaction, :title
 
+  def initialize(discount=0) #optionally takes an employee discount on initialization
+    @items = []
+    @total = 0 #sets an instance variable @total on initialization to zero
+    @discount = discount
+  end
+  
+  def total
+    @total
+  end   
+  
+  def add_item(title,price,quantity=1)
+    @title = title
+    @price = price
+    @total += price * quantity
+    @last_transaction = @total
+  
+    if quantity > 1 
+       counter = 0 
+      while counter < quantity 
+       @items << title
+       counter += 1
+     end
+   else
+    @items << title
+    end
+  end 
+  
+  def apply_discount
+    if discount > 0 
+    new_price = (price*discount)/100
+    @total -= new_price
+    "After the discount, the total comes to $#{@total}."
+    else 
+    "There is no discount to apply."
+    end 
+  end
+
+  def void_last_transaction
+    @total -= @price 
+    @last_transaction  = 0.0
+    #binding.pry
+  end
+end 
